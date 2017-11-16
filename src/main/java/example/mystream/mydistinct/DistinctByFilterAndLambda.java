bf107263ef3a2cf67cb0ef6a28013fef96feb03d
puppylpg <shininglhb@163.com>
2017-11-17 01:31:54 +0800

    2 ways: Wrapper & filter() + lambda, to accomplish distinct()

---
+++ src/main/java/example/mystream/mydistinct/DistinctByFilterAndLambda.java
@@ -0,0 +1,57 @@
+package example.mystream.mydistinct;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * @author liuhaibo on 2017/11/17
+ */
+public class DistinctByFilterAndLambda {
+
+    /**
+     * Assume that VideoInfo is a class that we can't modify
+     */
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @ToString
+    private static class VideoInfo {
+        @Getter
+        String id;
+        int width;
+        int height;
+    }
+
+    public static void main(String[] args) {
+        List<VideoInfo> list = Arrays.asList(new VideoInfo("123", 1, 2),
+                new VideoInfo("456", 4, 5), new VideoInfo("123", 1, 2));
+
+        // Get distinct only
+        Map<String, VideoInfo> id2VideoInfo = list.stream().filter(distinctByKey(vi -> vi.getId())).collect(
+                Collectors.toMap(VideoInfo::getId, x -> x,
+                        (oldValue, newValue) -> newValue)
+        );
+
+        id2VideoInfo.forEach((x, y) -> System.out.println("<" + x + ", " + y + ">"));
+    }
+
+    /**
+     * If a key could not be put into ConcurrentHashMap, that means the key is duplicated
+     * @param keyExtractor a mapping function to produce keys
+     * @param <T> the type of the input elements
+     * @return true if key is duplicated; else return false
+     */
+    private static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
+        Map<Object, Boolean> map = new ConcurrentHashMap<>();
+        return t -> map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
+}
---
+++ src/main/java/example/mystream/mydistinct/DistinctByWrapper.java
@@ -0,0 +1,79 @@
+package example.mystream.mydistinct;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author liuhaibo on 2017/11/17
+ */
+public class DistinctByWrapper {
+
+    private static class VideoInfoWrapper {
+
+        private final VideoInfo videoInfo;
+
+        public VideoInfoWrapper(VideoInfo videoInfo) {
+            this.videoInfo = videoInfo;
+        }
+
+        public VideoInfo unwrap() {
+            return videoInfo;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof VideoInfo)) {
+                return false;
+            }
+            VideoInfo vi = (VideoInfo) obj;
+            return videoInfo.id.equals(vi.id)
+                    && videoInfo.width == vi.width
+                    && videoInfo.height == vi.height;
+        }
+
+        @Override
+        public int hashCode() {
+            int n = 31;
+            n = n * 31 + videoInfo.id.hashCode();
+            n = n * 31 + videoInfo.height;
+            n = n * 31 + videoInfo.width;
+            return n;
+        }
+    }
+
+    public static void main(String [] args) {
+        List<VideoInfo> list = Arrays.asList(new VideoInfo("123", 1, 2),
+                new VideoInfo("456", 4, 5), new VideoInfo("123", 1, 2));
+
+        // VideoInfo --map()--> VideoInfoWrapper ----> distinct(): VideoInfoWrapper --map()--> VideoInfo
+        Map<String, VideoInfo> id2VideoInfo = list.stream()
+                .map(VideoInfoWrapper::new).distinct().map(VideoInfoWrapper::unwrap)
+                .collect(
+                Collectors.toMap(VideoInfo::getId, x -> x,
+                        (oldValue, newValue) -> newValue)
+        );
+
+        id2VideoInfo.forEach((x, y) -> System.out.println("<" + x + ", " + y + ">"));
+    }
+
+}
+
+/**
+ * Assume that VideoInfo is a class that we can't modify
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+class VideoInfo {
+    @Getter
+    String id;
+    int width;
+    int height;
+}
