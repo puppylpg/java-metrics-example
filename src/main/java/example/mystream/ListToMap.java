42b82fa4e63c35cbdd90be40652f34f5f70589a3
puppylpg <shininglhb@163.com>
2017-11-16 22:10:28 +0800

    optimize hashCode() & use static inner class

--- src/main/java/example/mystream/ListToMap.java
+++ src/main/java/example/mystream/ListToMap.java
@@ -15,6 +15,49 @@ import java.util.stream.Collectors;
  */
 public class ListToMap {
 
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @ToString
+    private static class VideoInfo {
+        @Getter
+        String id;
+        int width;
+        int height;
+
+        public static void main(String [] args) {
+            System.out.println(new VideoInfo("123", 1, 2).equals(new VideoInfo("123", 1, 2)));
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof VideoInfo)) {
+                return false;
+            }
+            VideoInfo vi = (VideoInfo) obj;
+            return this.id.equals(vi.id)
+                    && this.width == vi.width
+                    && this.height == vi.height;
+        }
+
+        /**
+         * If equals() is override, hashCode() must be override, too.
+         * 1. if a equals b, they must have the same hashCode;
+         * 2. if a doesn't equals b, they may have the same hashCode;
+         * 3. hashCode written in this way can be affected by sequence of the fields;
+         * 3. 2^5 - 1 = 31. So 31 will be faster when do the multiplication,
+         *      because it can be replaced by bit-shifting: 31 * i = (i << 5) - i.
+         * @return
+         */
+        @Override
+        public int hashCode() {
+            int n = 31;
+            n = n * 31 + this.id.hashCode();
+            n = n * 31 + this.height;
+            n = n * 31 + this.width;
+            return n;
+        }
+    }
+
     public static void main(String [] args) {
         List<VideoInfo> list = Arrays.asList(new VideoInfo("123", 1, 2),
                 new VideoInfo("456", 4, 5), new VideoInfo("123", 1, 2));
@@ -37,40 +80,5 @@ public class ListToMap {
 
         System.out.println("No Duplicated2: ");
         id2VideoInfo2.forEach((x, y) -> System.out.println("<" + x + ", " + y + ">"));
-    }
-}
-
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-class VideoInfo {
-    @Getter
-    String id;
-    int width;
-    int height;
-
-    public static void main(String [] args) {
-        System.out.println(new VideoInfo("123", 1, 2).equals(new VideoInfo("123", 1, 2)));
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof VideoInfo) {
-            VideoInfo vi = (VideoInfo) obj;
-            if (this.id.equals(vi.id)
-                    && this.width == vi.width
-                    && this.height == vi.height) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // if a equals b, they must have the same hashCode
-    // if a doesn't equals b, they may have the same hashCode
-    @Override
-    public int hashCode() {
-        return this.id.hashCode() * 37
-                + this.height + this.width;
     }
 }
