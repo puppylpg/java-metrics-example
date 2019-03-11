dc8510a114d3665b1341073d9db9bd74d661d8cd
puppylpg <shininglhb@163.com>
2019-03-11 16:41:22 +0800

    Simplify zk callback.

--- src/main/java/other/zk/DataMonitor.java
+++ src/main/java/other/zk/DataMonitor.java
@@ -17,14 +17,17 @@ import org.apache.zookeeper.data.Stat;
  * zk的回调one-time trigger！！
  *
  * 流程：
- * A. DataMonitor检查znode的exists，设置“完成回调”，当检查完毕时，触发“完成回调”processResult，
+ * 完成回调：DataMonitor检查znode的exists，设置“完成回调”，当检查完毕时，触发“完成回调”processResult，
  * （同时也给该znode设置了“watch回调”）
- * 如果zk状态正常，调用getData，若发现数据更新，重启linux command；
- * 若发现zk超时、无法认证，结束程序；否则尝试再用exists检查（再设置新的完成回调和watch回调）
+ * 如果zk状态正常，啥也不做；
+ * 若发现zk超时、无法认证，结束程序；
+ * 否则尝试再用exists检查（再设置新的完成回调和watch回调）
  *
- * B. 如果znode变了，会触发{@link ZkCommander}的“watch回调”，委托给{@link DataMonitor}：
+ * watch回调：如果znode变了，会触发{@link ZkCommander}的“watch回调”，委托给{@link DataMonitor}：
  * 如果是session超时，结束进程；
- * 如果是其他变动，用exists检查该znode，设置完成回调和watch回调，回到流程A。（流程A有getData的逻辑。。。）
+ * 如果是znode数据变了（getData，并重新创建watch），重启linux command；
+ * 如果是znode删了/新增了，打log，重新创建watch（必须重新创建，要不然下次再变就收不到通知了）；
+ * 如果是znode的其他事件，重新检查znode。
  *
  * 所以其实就是，每当znode变了，将{@link WatchedEvent}通知watcher，
  * 然后watcher根据{@link WatchedEvent}，做出进一步判断。
@@ -45,8 +48,6 @@ public class DataMonitor implements Watcher, StatCallback {
 
     private DataMonitorListener listener;
 
-    private byte prevData[];
-
     DataMonitor(ZooKeeper zk, String znode, Watcher chainedWatcher, DataMonitorListener listener) {
         this.zk = zk;
         this.znode = znode;
@@ -64,29 +65,53 @@ public class DataMonitor implements Watcher, StatCallback {
     @Override
     public void process(WatchedEvent event) {
         String path = event.getPath();
-        if (event.getType() == Event.EventType.None) {
-            // We are being told that the state of the
-            // connection has changed
-            switch (event.getState()) {
-                case SyncConnected:
-                    // In this particular example we don't need to do anything
-                    // here - watches are automatically re-registered with
-                    // server and any watches triggered while the client was
-                    // disconnected will be delivered (in order of course)
-                    break;
-                case Expired:
-                    // It's all over
-                    dead = true;
-                    listener.closing(KeeperException.Code.SessionExpired);
-                    break;
-            }
-        } else {
-            if (path != null && path.equals(znode)) {
-                // Something has changed on the node, let's find out
-                // 看看我关心的这个znode到底怎么变了！顺便再注册一次完成回调和watch回调
+        switch (event.getType()) {
+            case None:
+                // We are being told that the state of the
+                // connection has changed
+                switch (event.getState()) {
+                    case SyncConnected:
+                        // In this particular example we don't need to do anything
+                        // here - watches are automatically re-registered with
+                        // server and any watches triggered while the client was
+                        // disconnected will be delivered (in order of course)
+                        System.out.println("Every thing is ok for whole zk or node: " + path);
+                        break;
+                    case Expired:
+                        // It's all over
+                        dead = true;
+                        System.out.println("Session expired, quit.");
+                        listener.closing(KeeperException.Code.SessionExpired);
+                        break;
+                }
+                break;
+            case NodeDataChanged:
+                System.out.println("Node data changed: " + path);
+                byte[] b = null;
+                try {
+                    b = zk.getData(znode, true, null);
+                } catch (KeeperException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (b != null) {
+                    listener.nodeChange(b);
+                }
+                break;
+            case NodeDeleted:
+                System.out.println("Woops, node deleted: " + path);
                 zk.exists(znode, true, this, null);
-            }
+                break;
+            case NodeCreated:
+                System.out.println("Wow, new node created: " + path);
+                zk.exists(znode, true, this, null);
+                break;
+            case NodeChildrenChanged:
+            default:
+                System.out.println("Others things which I don't care happened for the node: " + path);
+                System.out.println("Don't know, let me find out..");
+                zk.exists(znode, true, this, null);
         }
+
         // 如果我们把DataMonitor也注册为chainedWatcher，就无限递归stackoverflow了
         if (chainedWatcher != null) {
             chainedWatcher.process(event);
@@ -95,41 +120,23 @@ public class DataMonitor implements Watcher, StatCallback {
 
     @Override
     public void processResult(int rc, String path, Object ctx, Stat stat) {
-        boolean exists;
         switch (rc) {
             case Code.Ok:
-                exists = true;
+                System.out.println("Node exists: " + path);
                 break;
             case Code.NoNode:
-                exists = false;
+                System.out.println("No node: " + path);
                 break;
             case Code.SessionExpired:
             case Code.NoAuth:
+                System.out.println("No Auth or SessionExpired, quit.");
                 dead = true;
                 listener.closing(rc);
                 return;
             default:
+                System.out.println("Unknown completion callback status. retry.");
                 // Retry errors：可恢复性错误，尝试恢复一发
                 zk.exists(znode, true, this, null);
-                return;
-        }
-
-        byte b[] = null;
-        if (exists) {
-            try {
-                b = zk.getData(znode, false, null);
-            } catch (KeeperException e) {
-                // We don't need to worry about recovering now. The watch
-                // callbacks will kick off any exception handling
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                return;
-            }
-        }
-        if ((b == null && b != prevData)
-                || (b != null && !Arrays.equals(prevData, b))) {
-            listener.nodeChange(b);
-            prevData = b;
         }
     }
 
